0877dca5fad9d01e33bc687fcc4882fd5062182b
Raihan Siyun <rsiyun@gmail.com>
2024-05-15 12:57:06 +0700

    migration for user

--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->enum("role",["Admin", "Kasir", "ShopEmloyee"]);
+            $table->boolean("isActive");
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
