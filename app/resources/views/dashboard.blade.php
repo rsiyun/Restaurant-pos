9b8ba4f3dc13507929f7e0de8e06246f9fc7080f
Yoga Wilanda <hyoga.wilanda@gmail.com>
2024-05-16 17:57:12 +0700

    feat: add card and circular button component

--- app/resources/views/dashboard.blade.php
+++ app/resources/views/dashboard.blade.php
@@ -4,28 +4,19 @@
             {{ __('Dashboard') }}
         </h2>
 
-<<<<<<< HEAD
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         {{ __("You're logged in!") }}
                     </div>
                 </div>
             </div>
         </div>
-    </x-slot>
-=======
-    <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <script />
->>>>>>> cd856c5b2336091929e79a4d9fe35254a6c99764
+        @include('components.cards.cards-2', ['name' => 'Card 1', 'content' => 'Content 1'])
+
+       
+
+    </x-slot>
 </x-app-layout>
