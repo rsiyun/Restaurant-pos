a493f6a5f6d6d908916b0c573d766056851f5341
Dito-7 <112498814+Dito-7@users.noreply.github.com>
2024-05-18 09:54:17 +0700

    fix : migration user role

--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,7 +16,7 @@ return new class extends Migration
             $table->string('slug')->unique();
             $table->string('name');
             $table->string('email')->unique();
-            $table->enum("role",["Admin", "Kasir", "ShopEmloyee"]);
+            $table->enum("role", ["Admin", "Kasir", "ShopEmployee"]);
             $table->boolean("isActive");
             $table->string('password');
             $table->timestamps();
