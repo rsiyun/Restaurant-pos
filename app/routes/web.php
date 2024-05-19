d704ef48a7ec292e8b4f9a7c0a3011a8e8a65084
Raihan Siyun <rsiyun@gmail.com>
2024-05-19 16:13:58 +0700

    feat : login frontend

--- app/routes/web.php
+++ app/routes/web.php
@@ -29,7 +29,7 @@ Route::get('/tambah-produk', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware("checkRole:Admin,Kasir,ShopEmloyee")->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
