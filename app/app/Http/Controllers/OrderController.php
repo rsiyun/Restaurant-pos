ce156dcf4d84178e6fefba08540afc3e74e137ff
Raihan Siyun <rsiyun@gmail.com>
2024-05-20 16:33:58 +0700

    feat : initial order

---
+++ app/app/Http/Controllers/OrderController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\FrontEnd;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Http;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+
+    }
+    public function create(){
+
+    }
+    public function edit(){
+
+    }
+    public function show($id){
+
+    }
+    public function update($id){
+
+    }
+    public function store(){
+
+    }
+    public function destroy($id){
+
+    }
+}
