cd99d1c0c682a502a325321406c29a34bc7e5961
Yoga Wilanda <hyoga.wilanda@gmail.com>
2024-05-22 13:01:31 +0700

    fix : refactor side nav button

---
+++ app/resources/views/components/buttons/side-navbar.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ $url ?? '#' }}"
+    class="block px-4 py-2 mt-4 transition hover:bg-white hover:text-black hover:duration-500 hover:ease-in-out">
+    {{ $title ? $title : 'Untitled Menu Button' }}
+</a>
