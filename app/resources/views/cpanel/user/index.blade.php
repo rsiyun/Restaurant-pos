698bcb2b21c0bace5281c86ea114152496e52ced
Yoga Wilanda <hyoga.wilanda@gmail.com>
2024-05-24 20:06:25 +0700

    update dashboard admin of user

--- app/resources/views/cpanel/user/index.blade.php
+++ app/resources/views/cpanel/user/index.blade.php
@@ -1,39 +1,47 @@
 @extends('cpanel.layout.app')
 
 @section('content')
-    {{-- @include('components.receipts.receipts', [
-        'type' => 'success',
-        'message' => 'This is a success message',
-    ]) --}}
-
-    {{-- @include('components.receipts.receipts', [
-        'type' => 'failed',
-        'message' => 'This is a failed message',
-    ]) --}}
-
+    {{-- ! HELP! ini harusnya kalo pakai breeze tinggal panggil auth()->user --}}
     <div class="pb-8">
-        <h2 class="text-2xl font-bold text-right text-blue-600 border">
-            Hai, {{ $name ?? 'Developer' }}!
+        <h2 class="text-2xl font-bold text-right text-blue-600">
+            Hai, {{ $users['name'] ?? 'Developer' }}!
         </h2>
     </div>
 
     {{-- Judul Section --}}
-    <h1 class="mt-6 text-3xl font-bold text-blue-600">
-        Dashboard - User
+    <h1 class="flex-col mt-6 text-3xl text-blue-600 font-extralight">
+        Dashboard
+        <h2 class="text-2xl font-bold text-slate-700">
+            User
+        </h2>
     </h1>
 
     {{-- border separator --}}
     <div class="my-4 border-b border-gray-300"></div>
 
-    <div class="flex flex-row justify-end">
+    {{-- Section Controls --}}
+    <div class="flex flex-row justify-start gap-5 pb-5">
+        <select name="role" id="role"
+            class="px-4 py-2 text-black border-blue-500 rounded-md shadow-sm appearance-none min-w-2 focus:outline-none focus:ring-1 w-[12rem]">
+            <option value="" disabled selected>Sortir berdasarkan</option>
+            <option value="admin">Nama</option>
+            <option value="user">Email</option>
+            <option value="status">Status</option>
+        </select>
+
         <a href="{{ url('/dashboard/user/create') }}" class="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700">
             Tambah User
         </a>
     </div>
+
     {{-- Table untuk informasi user --}}
     <table class="w-full text-left border-collapse rounded table-auto">
         <thead>
-            <t>
+            <tr>
+                <th
+                    class="px-4 py-3 font-medium leading-4 tracking-wider text-black border bg-blue text-md text-2xluppercase border-slate-400 dark:bg-blue">
+                    No
+                </th>
                 <th
                     class="px-4 py-3 font-medium leading-4 tracking-wider text-black border bg-blue text-md text-2xluppercase border-slate-400 dark:bg-blue">
                     Nama
@@ -46,13 +54,24 @@
                     Role</th>
                 <th
                     class="px-4 py-3 font-medium leading-4 tracking-wider text-black bg-white border text-md text-2xluppercase border-slate-400 dark:bg-white">
-                    Status</th>
-            </t>
+                    Status
+                </th>
+                <th
+                    class="px-4 py-3 font-medium leading-4 tracking-wider text-black bg-white border text-md text-2xluppercase border-slate-400 dark:bg-white">
+                    Action
+                </th>
+
+
+            </tr>
         </thead>
         <tbody>
             @if (isset($users))
                 @foreach ($users as $user)
                     <tr class="border border-slate-400">
+                        <td class="px-4 py-3">
+                            {{ $loop->iteration ?? 0 }}
+                        </td>
+
                         <td class="px-4 py-3">
                             {{ $user['name'] ?? 'Unknown Status' }}
                         </td>
@@ -65,9 +84,29 @@
                             {{ $user['role'] ?? 'Unknown Status' }}
                         </td>
 
-                        <td id="status" class="px-4 py-3">
-                            {{ $user['role'] ?? 'Unknown Status' }}
+                        <td id="status"
+                            class="w-[3rem] px-4 py-3 text-center {{ $user['isActive'] = 1 ? 'text-green-500' : 'bg-orange-500' }}">
+                            <span class="text-xl">
+                                {{ $user['isActive'] = 1 ? '✅' : '❌' }}
+                            </span>
                         </td>
+
+
+                        <td id="tableaction" class="w-[5rem] px-4 py-3">
+                            <div class="flex gap-4">
+                                <a href="{{ url('/dashboard/user') }}{{ '/' . $user['slug'] . '/edit' }}"
+                                    class="px-4 py-2 text-white border rounded">✏️</a>
+
+                                <form action="{{ url('/dashboard/user/' . $user['slug']) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="px-4 py-2 text-white bg-red-600 border rounded"
+                                        onclick="return confirm('Apakah Anda yakin ingin menghapus user ini?')">🪣</button>
+                                </form>
+                            </div>
+
+                        </td>
+
                     </tr>
                 @endforeach
             @else
