42ee3af87dade661955e70b28d9e975129a4fd68
Yoga Wilanda <hyoga.wilanda@gmail.com>
2024-05-25 19:27:15 +0700

    update show item toko

--- app/resources/views/clients/show.blade.php
+++ app/resources/views/clients/show.blade.php
@@ -1,53 +1,61 @@
 @extends('layouts.app')
 
 @section('slot')
-    <div class="flex flex-col h-screen">
-        <div class="mt-[1rem] mb-[1rem]">
+    <div class="min-h-screen py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="mb-4">
             @include('components.h1-component', [
                 'slot' => 'Nama Makanan 1',
                 'bg' => '',
                 'textColor' => 'black',
             ])
         </div>
-        <div class="mb-12">
-
-        </div>
-        <div class="grid grid-cols-3 gap-4 grid-col max-w-7xl sm:px-6 lg:px-8">
-            {{-- CARAOUSEL --}}
-            <div class="inline-flex flex-1 w-12 h-12 bg-red-500">
-                <img src="{{ asset('img/logo/Group 11.png') }}" alt="">
+        <div class="mb-12"></div>
+        <div class="grid grid-cols-3 gap-4 max-w-7xl sm:px-6 lg:px-8">
+            {{-- CAROUSEL --}}
+            <div class="overflow-hidden bg-red-800 w-3/10  h-[250px]  items-center rounded-md border-2 border-red-500">
+                @if (isset($image))
+                    <img src="{{ asset('img/logo/Group 11.png') }}" alt="" class="object-cover w-full h-full">
+                @else
+                    <div class="flex items-center justify-center w-full h-full"></div>
+                @endif
             </div>
 
-            {{-- CARD PRODUCT --}}
-            <div id="texts" class="">
-                {{-- Image --}}
-                {{-- Image Description --}}
+            {{-- PRODUCT DETAILS --}}
+            <div id="texts" class="flex flex-col justify-between w-4/10">
+                <div>
+                    <p class="mb-2 text-4xl font-bold">
+                        Title
+                    </p>
+                    <p class="mb-4 text-xl text-gray-600">
+                        Price
+                    </p>
+                    <p class="text-gray-500 text-md">
+                        Stock
+                    </p>
+                </div>
 
-                <p class="text-4xl font-bold">
-                    Title
-                </p>
-
-                <p>
-                    Price
-                </p>
-
-                <p>
-                    Stock
-                </p>
+                <div>
+                    {{-- Action button --}}
+                    <div class="mb-4">
+                        <button class="p-2 text-white bg-green-500 rounded-md hover:bg-green-600">
+                            Edit
+                        </button>
+                        <button class="p-2 text-white bg-red-500 rounded-md hover:bg-red-600">
+                            Delete
+                        </button>
+                    </div>
+                </div>
             </div>
 
-            <div id="control" class="inline-flex flex-col bg-white border shadow flex-2">
-                <p>
+            {{-- CONTROL PANEL --}}
+            <div id="control" class="flex flex-col justify-between p-4 bg-white border rounded-md shadow-md w-[20rem]">
+                <p class="mb-4">
                     Atur jumlah yang diinginkan
                 </p>
-
-                <input type="number">
-
-
-                <button>
+                <input type="number" class="p-2 mb-4 border rounded-md" placeholder="Jumlah">
+                <button class="p-2 text-white bg-green-500 rounded-md hover:bg-green-600">
                     Tambahkan ke keranjang
                 </button>
-
             </div>
         </div>
     </div>
