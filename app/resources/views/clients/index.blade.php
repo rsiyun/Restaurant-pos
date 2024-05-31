5185312ed93fdb0ff4806679b49895e907d79c86
eca <116784250+sodiumKlorida@users.noreply.github.com>
2024-05-31 22:12:18 +0700

    fix: index shop employ

--- app/resources/views/clients/index.blade.php
+++ app/resources/views/clients/index.blade.php
@@ -1,27 +1,51 @@
 @extends('clients.layouts.app')
 
-    @section('slot')
-        <div class="px-4 mx-auto space-y-3 max-w-7xl sm:px-6 lg:px-8 min-h-[85vh]">
-            <div class="mt-[2rem] mb-[1rem]">
-                @include('components.h1-component', [
-                    'slot' => 'Menu Makanan',
-                    'bg' => 'amber-500',
-                    ])
+@section('slot')
+    <div class="px-4 mx-auto space-y-3 max-w-7xl sm:px-6 lg:px-8 min-h-[85vh]">
+
+        <div class="border flex justify-between mt-8">
+
+            <div class="flex flex-col gap-4 w-[13rem]">
+                <select id="" name=""
+                    class="border-gray-300  rounded-sm shadow-sm focus:border-blue-500 focus:ring-blue-500">
+                    <option class="rounded-sm" value="1">Kategori</option>
+                    <option class="rounded-sm" value="1">Makanan</option>
+                    <option class="rounded-sm" value="2">Minuman</option>
+                    <option class="rounded-sm" value="3">Jajan</option>
+                </select>
             </div>
-            <div class="pb-8">
-                <div class="flex flex-wrap items-center justify-center">
-                    @foreach ($products as $product)
+
+            <div class="flex justify-center content-center items-middle gap-6">
+                <input type="text" class="w-[20rem] h-10 bg-gray-200 rounded-sm">
+
+                <button
+                    class="px-4 py-2 font-bold text-white transition w-[6rem] duration-300 ease-in-out bg-sky-900 rounded-sm hover:drop-shadow-lg">
+                    Search
+                </button>
+            </div>
+
+            <div class="">
+            @include('components.buttons.btn-all',[
+                'title' => 'Tambah +',
+            ])
+
+            </div>
+        </div>
+
+        <div class="pb-8">
+            <div class="flex flex-wrap items-center justify-center">
+                @foreach ($products as $product)
                     <div class="px-8">
                         @include('components.small-card-content', [
-                            'foto' => SessionService::image_url().$product["productImage"],
-                            'name' => $product["productName"],
-                            'content' => "Rp. ".$product["productPrice"],
-                            'link' => url("/product"."/". $product['slug'])
-                            ])
+                            'foto' => SessionService::image_url() . $product['productImage'],
+                            'name' => $product['productName'],
+                            'content' => 'Rp. ' . $product['productPrice'],
+                            'link' => url('/product' . '/' . $product['slug']),
+                        ])
                     </div>
                 @endforeach
 
             </div>
         </div>
-        </div>
-        @endsection
+    </div>
+@endsection
--- app/resources/views/cpanel/shop/edit.blade.php
+++ app/resources/views/cpanel/shop/edit.blade.php
@@ -4,14 +4,6 @@
     <form action="{{ url('dashboard/shop/' . $slug) }}" method="POST">
         @csrf
         @method('PUT')
-        {{-- <div>
-            <label for="ownerName">Buyer Name:</label>
-            <input type="text" id="ownerName" name="ownerName" required value="{{ $ownerName }}">
-        </div>
-        <div>
-            <label for="shopName">Buyer Name:</label>
-            <input type="text" id="shopName" name="shopName" required value="{{ $shopName }}">
-        </div> --}}
 
         <div class="flex flex-col gap-1 mt-[20px]">
             @include('components.input-label', [
