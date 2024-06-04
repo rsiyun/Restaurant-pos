39379d561d692694c62d0662aeb1549a55dfeeda
Dito-7 <112498814+Dito-7@users.noreply.github.com>
2024-06-04 15:06:04 +0700

    feat : add data tables to Kasir and Admin

--- app/resources/views/cpanel/order/index.blade.php
+++ app/resources/views/cpanel/order/index.blade.php
@@ -1,6 +1,6 @@
-@extends("cpanel.layout.app")
+@extends('cpanel.layout.app')
 
-@section("content")
+@section('content')
     <h1 class="flex-col mt-6 text-3xl text-blue-600 font-extralight">
         Dashboard
         <h2 class="text-2xl font-bold text-slate-700">
@@ -14,74 +14,79 @@
         </a>
     </div>
     <div>
-        <table class="w-full text-left border-collapse rounded table-auto">
-            <thead>
-                <tr>
-                    <th
-                        class="px-4 py-3 font-medium leading-4 tracking-wider text-black border bg-blue text-md text-2xluppercase border-slate-400 dark:bg-blue">
-                        No
-                    </th>
-                    <th
-                        class="px-4 py-3 font-medium leading-4 tracking-wider text-black border bg-blue text-md text-2xluppercase border-slate-400 dark:bg-blue">
-                        Id order
-                    </th>
-                    <th
-                        class="px-4 py-3 font-medium leading-4 tracking-wider text-black bg-white border text-md text-2xluppercase border-slate-400 dark:bg-white">
-                        Nama Pembeli</th>
-                    <th
-                        class="px-4 py-3 font-medium leading-4 tracking-wider text-black bg-white border text-md text-2xluppercase border-slate-400 dark:bg-white">
-                        Total</th>
-                    <th
-                        class="px-4 py-3 font-medium leading-4 tracking-wider text-black bg-white border text-md text-2xluppercase border-slate-400 dark:bg-white">
-                        Nama Kasir
-                    </th>
-                    <th
-                        class="px-4 py-3 font-medium leading-4 tracking-wider text-black bg-white border text-md text-2xluppercase border-slate-400 dark:bg-white">
-                        Action
-                    </th>
+        <x-tables.table>
 
-
-                </tr>
-            </thead>
-            <tbody>
-
-                @if (isset($data))
-                    @foreach ($data as $order)
-                        <tr class="border border-slate-400">
-                            <td class="px-4 py-3">
-                                {{ $loop->iteration ?? 0 }}
-                            </td>
-
-                            <td class="px-4 py-3">
-                                {{ $order['slug'] ?? 'Unknown Status' }}
-                            </td>
-
-                            <td class="px-4 py-3">
-                                {{ $order['buyerName'] ?? 'Unknown Status' }}
-                            </td>
-
-                            <td class="px-4 py-3">
-                                {{ $order['totalOrder'] ?? 'Unknown Status' }}
-                            </td>
-                            <td class="px-4 py-3">
-                                {{ $order['kasir']["name"] ?? 'Unknown Status' }}
-                            </td>
-
-                            <td id="tableaction" class="w-[5rem] px-4 py-3">
-                                <a href="{{ url('/dashboard/order', [$order['slug']]) }}" class="px-4 py-2 text-white bg-green-500 rounded">Detail</a>
-                            </td>
-
-                        </tr>
-                    @endforeach
-                @else
+            <table id="table" class="w-full text-left border-collapse rounded table-auto">
+                <thead>
                     <tr>
-                        <td class="px-4 py-3 text-center">No orders found.</td>
+                        <th
+                            class="px-4 py-3 font-medium leading-4 tracking-wider text-black border bg-blue text-md text-2xluppercase border-slate-400 dark:bg-blue">
+                            No
+                        </th>
+                        <th
+                            class="px-4 py-3 font-medium leading-4 tracking-wider text-black border bg-blue text-md text-2xluppercase border-slate-400 dark:bg-blue">
+                            Id order
+                        </th>
+                        <th
+                            class="px-4 py-3 font-medium leading-4 tracking-wider text-black bg-white border text-md text-2xluppercase border-slate-400 dark:bg-white">
+                            Nama Pembeli</th>
+                        <th
+                            class="px-4 py-3 font-medium leading-4 tracking-wider text-black bg-white border text-md text-2xluppercase border-slate-400 dark:bg-white">
+                            Total</th>
+                        <th
+                            class="px-4 py-3 font-medium leading-4 tracking-wider text-black bg-white border text-md text-2xluppercase border-slate-400 dark:bg-white">
+                            Nama Kasir
+                        </th>
+                        <th
+                            class="px-4 py-3 font-medium leading-4 tracking-wider text-black bg-white border text-md text-2xluppercase border-slate-400 dark:bg-white">
+                            Action
+                        </th>
+
+
                     </tr>
-                @endif
-            </tbody>
+                </thead>
+                <tbody>
+
+                    @if (isset($data))
+                        @foreach ($data as $order)
+                            <tr class="border border-slate-400">
+                                <td class="px-4 py-3">
+                                    {{ $loop->iteration ?? 0 }}
+                                </td>
+
+                                <td class="px-4 py-3">
+                                    {{ $order['slug'] ?? 'Unknown Status' }}
+                                </td>
+
+                                <td class="px-4 py-3">
+                                    {{ $order['buyerName'] ?? 'Unknown Status' }}
+                                </td>
+
+                                <td class="px-4 py-3">
+                                    {{ $order['totalOrder'] ?? 'Unknown Status' }}
+                                </td>
+                                <td class="px-4 py-3">
+                                    {{ $order['kasir']['name'] ?? 'Unknown Status' }}
+                                </td>
+
+                                <td id="tableaction" class="w-[5rem] px-4 py-3">
+                                    <a href="{{ url('/dashboard/order', [$order['slug']]) }}"
+                                        class="px-4 py-2 text-white bg-green-500 rounded">Detail</a>
+                                </td>
+
+                            </tr>
+                        @endforeach
+                    @else
+                        <tr>
+                            <td class="px-4 py-3 text-center">No orders found.</td>
+                        </tr>
+                    @endif
+                </tbody>
 
 
-        </table>
+            </table>
+        </x-tables.table>
+
     </div>
 
 
