9004d9fbfcc9585cce161e23aaecad4947ef9034
Raihan Siyun <rsiyun@gmail.com>
2024-06-09 13:49:57 +0700

    feat add throtle and authorization

--- api/routes/api.php
+++ api/routes/api.php
@@ -25,62 +25,50 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => ['auth:sanctum', 'checkrole:Admin,ShopEmployee']], function () {
-    Route::get("product", [ProductController::class, 'index']);
-    Route::get("product/{product}", [ProductController::class, 'show']);
-    Route::get("productByShop/{product}", [ProductController::class, 'showByShop']);
-    Route::post("product", [ProductController::class, 'store']);
-    Route::put("product/{product}", [ProductController::class, 'update']);
-
-    Route::get("ticketByShop/{shop}", [TicketsController::class, 'ticketByShop']);
-    Route::get("shop/{shop}", [ShopController::class, 'show']);
-    Route::post("ticket", [TicketsController::class, 'store']);
-});
-Route::group(['middleware' => ['auth:sanctum', 'checkrole:Admin,Kasir']], function () {
-    Route::get("order", [OrdersController::class, 'index']);
-    Route::get("order/{order}", [OrdersController::class, 'show']);
-    Route::post("order", [OrdersController::class, 'store']);
-    Route::delete("order/{order}", [OrdersController::class, 'destroy']);
-    Route::put("order/{order}", [OrdersController::class, 'update']);
-});
-
-Route::middleware('auth:sanctum')->group(function () {
-
-
-    // Route::get("shop", [ShopController::class, 'index']);
-    // Route::get("shop/{shop}", [ShopController::class, 'show']);
-    // Route::post("shop", [ShopController::class, 'store']);
-    // Route::put("shop/{shop}", [ShopController::class, 'update']);
-    // Route::delete("shop/{shop}", [ShopController::class, 'destroy']);
-
-});
-
-Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/logout', [AuthController::class, 'logout']);
-Route::get('/user/token', [AuthController::class, "showWithToken"]);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user/token', [AuthController::class, "showWithToken"]);
+    Route::group(['middleware' => ['checkrole:Admin,ShopEmployee']], function () {
+        Route::get("product", [ProductController::class, 'index']);
+        Route::get("product/{product}", [ProductController::class, 'show']);
+        Route::get("productByShop/{product}", [ProductController::class, 'showByShop']);
+        Route::post("product", [ProductController::class, 'store']);
+        Route::put("product/{product}", [ProductController::class, 'update']);
+        Route::put("ticket/{ticket}", [TicketsController::class, 'update']);
+        Route::get("ticketByShop/{shop}", [TicketsController::class, 'ticketByShop']);
+        Route::get("shop/{shop}", [ShopController::class, 'show']);
+        Route::post("ticket", [TicketsController::class, 'store']);
 
-// User
-Route::get("/dashboard", [UserController::class, "dashboard"]);
-Route::get("/user", [UserController::class, "index"]);
-Route::get("/user/{user}", [UserController::class, 'show']);
-Route::post("/user", [UserController::class, 'store']);
-Route::put("/user/{user}", [UserController::class, 'update']);
-Route::delete("/user/{user}", [UserController::class, 'destroy']);
+    });
+    Route::group(['middleware' => ['checkrole:Admin,Kasir']], function () {
+        Route::get("/dashboard", [UserController::class, "dashboard"]);
+        Route::get("order", [OrdersController::class, 'index']);
+        Route::get("/unpaymentTicket", [TicketsController::class, 'unpayment']);
+        Route::get("order/{order}", [OrdersController::class, 'show']);
+        Route::post("order", [OrdersController::class, 'store']);
+        Route::delete("order/{order}", [OrdersController::class, 'destroy']);
+        Route::put("order/{order}", [OrdersController::class, 'update']);
+    });
+    Route::group(['middleware' => ['checkrole:Admin']], function () {
+        Route::get("/user", [UserController::class, "index"]);
+        Route::get("/user/{user}", [UserController::class, 'show']);
+        Route::post("/user", [UserController::class, 'store']);
+        Route::put("/user/{user}", [UserController::class, 'update']);
+        Route::delete("/user/{user}", [UserController::class, 'destroy']);
+        Route::get("shop", [ShopController::class, 'index']);
+        Route::get("shop/{shop}", [ShopController::class, 'show']);
+        Route::post("shop", [ShopController::class, 'store']);
+        Route::put("shop/{shop}", [ShopController::class, 'update']);
+        Route::delete("shop/{shop}", [ShopController::class, 'destroy']);
 
-Route::get("shop", [ShopController::class, 'index']);
-Route::get("shop/{shop}", [ShopController::class, 'show']);
-Route::post("shop", [ShopController::class, 'store']);
-Route::put("shop/{shop}", [ShopController::class, 'update']);
-Route::delete("shop/{shop}", [ShopController::class, 'destroy']);
+    });
+});
 
-Route::get("ticket", [TicketsController::class, 'index']);
-Route::get("ticketByShop/{shop}", [TicketsController::class, 'ticketByShop']);
-Route::get("ticket/{ticket}", [TicketsController::class, 'show']);
-Route::post("ticket", [TicketsController::class, 'store']);
-Route::delete("ticket/{ticket}", [TicketsController::class, 'destroy']);
-Route::put("ticket/{ticket}", [TicketsController::class, 'update']);
-Route::get("/unpaymentTicket", [TicketsController::class, 'unpayment']);
+
+// Route::post('/register', [AuthController::class, 'register']);
+
+// Route::get("ticket", [TicketsController::class, 'index']);
 
 Route::get("ticketDetail", [TicketDetailsController::class, 'index']);
 Route::get("ticketDetail/{slug}", [TicketDetailsController::class, 'show']);
