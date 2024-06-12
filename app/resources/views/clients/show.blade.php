c37e3e9487bdb98f7db54057d873b9d0b03dd1a8
eca <116784250+sodiumKlorida@users.noreply.github.com>
2024-06-13 01:05:41 +0700

    fix: show produk client konsisten button

--- app/resources/views/clients/show.blade.php
+++ app/resources/views/clients/show.blade.php
@@ -5,7 +5,7 @@
         <div class="mb-12"></div>
         <div class="grid grid-cols-3 gap-4 max-w-7xl sm:px-6 lg:px-8">
             {{-- CAROUSEL --}}
-            <div class="overflow-hidden bg-red-800 w-3/10 h-[250px] items-center rounded-md border-2 border-red-500">
+            <div class="overflow-hidden bg-withe w-3/10 h-[250px] items-center rounded-md border-2 border-grey">
                 <img src="{{ url(SessionService::image_url() . $product['productImage']) }}" alt=""
                     class="object-cover w-full h-full">
             </div>
@@ -28,7 +28,7 @@
                     {{-- Action button --}}
                     <div class="mb-4">
 
-                        <a href="{{ url('product/' . $product['slug'] . '/edit') }}" class="p-2 text-white bg-green-500 rounded-md hover:bg-green-600">
+                        <a href="{{ url('product/' . $product['slug'] . '/edit') }}" class="py-3 px-4 text-white bg-blue-900 rounded-sm hover:drop-shadow-lg">
                             Edit
                         </a>
                     </div>
@@ -37,15 +37,15 @@
 
             {{-- CONTROL PANEL --}}
             <div id="control" class="flex flex-col justify-around p-4 bg-white border rounded-md shadow-md w-[20rem]">
-                <p class="mb-2">
+                <p class="mb-2 text-center">
                     Atur jumlah yang diinginkan
                 </p>
                 <form method="POST" action="{{ url('/cart/add/' . $product['slug']) }}">
                     @csrf
                     <div class="flex flex-col">
-                        <input type="number" name="quantity" class="p-2 mb-4 border rounded-md" placeholder="Jumlah"
+                        <input type="number" name="quantity" class="p-2 mb-4 border rounded-sm" placeholder="Jumlah"
                             min="1" max="{{ $product['productStock'] }}" required>
-                        <button type="submit" class="p-2 text-white bg-green-500 rounded-md hover:bg-green-600">
+                        <button type="submit" class="p-2 text-white bg-blue-900 rounded-sm hover:drop-shadow-lg">
                             Tambahkan ke keranjang
                         </button>
                     </div>
